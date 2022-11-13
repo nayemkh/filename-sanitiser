a6bdec7930c18ef1ad5a2810d315bbf1e9f0ad1f
Nayem <nayem-khan@hotmail.com>
2022-11-13 12:21:37 +0000

    don't even attempt to run processFilenames function unless filenames exist

--- controller.php
+++ controller.php
@@ -14,38 +14,36 @@ class Controller
 
     public function processFileNames()
     {
-        $submitted = $_POST['filenames'] ?? '';
-
-        if ($submitted) {
-            $submitted = htmlentities($submitted);
-            $filenames = array_filter(explode(PHP_EOL, $submitted));
-            if (is_array($filenames) && !empty($filenames)) {
-                foreach ($filenames as $filename) {
-                    if (isset($_POST['r_underscore']) && $_POST['r_underscore'] === '1') {
-                        $filename = str_replace('_', ' ', $filename);
-                    }
-
-                    if (isset($_POST['r_hyphen']) && $_POST['r_hyphen'] === '1') {
-                        $filename = str_replace('-', ' ', $filename);
-                    }
-
-                    if (isset($_POST['capitalise_word']) && $_POST['capitalise_word'] === '1') {
-                        $words = explode(' ', $filename);
-                        $capitalisedWords = [];
-                        foreach ($words as $word) {
-                            $capitalisedWords[] = ucfirst($word);
-                        }
-                        $filename = implode(' ', $capitalisedWords);
-                    }
-
-                    $this->filenames[] = $filename;
+        $submitted = htmlentities($_POST['filenames']);
+        $filenames = array_filter(explode(PHP_EOL, $submitted));
+        if (is_array($filenames) && !empty($filenames)) {
+            foreach ($filenames as $filename) {
+                if (isset($_POST['r_underscore']) && $_POST['r_underscore'] === '1') {
+                    $filename = str_replace('_', ' ', $filename);
                 }
+
+                if (isset($_POST['r_hyphen']) && $_POST['r_hyphen'] === '1') {
+                    $filename = str_replace('-', ' ', $filename);
+                }
+
+                if (isset($_POST['capitalise_word']) && $_POST['capitalise_word'] === '1') {
+                    $words = explode(' ', $filename);
+                    $capitalisedWords = [];
+                    foreach ($words as $word) {
+                        $capitalisedWords[] = ucfirst($word);
+                    }
+                    $filename = implode(' ', $capitalisedWords);
+                }
+
+                $this->filenames[] = $filename;
             }
         }
     }
 
     public function run()
     {
-        $this->processFileNames();
+        if (isset($_POST['filenames']) && trim($_POST['filenames'] !== '')) {
+            $this->processFileNames();
+        }
     }
 }
